7773df2bca8326e271b5c50d21238477acf8e906
nzakawa <zakawa1@gmail.com>
2022-11-18 13:27:23 -0800

    shell

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main(void);
+int main(int ac, char **av);
+
+
+#endif
